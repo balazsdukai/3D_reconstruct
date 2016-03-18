6c7818ec15bc75a21501905b68e223f182baaf7f
Balázs Dukai <balazs.dukai@gmail.com>
2016-03-18 15:53:29 +0100

    Initial commit. Contains Assingment 2.

---
+++ FDN.py
@@ -0,0 +1,110 @@
+"""  
+Just some alternative ideas...
+ 
+for pt in range(len(PC)): 
+    RG.Circle(pt, R)
+    RG.Curve.Contains(testPoint)
+        
+create circle with pt as center and R as radius
+which points fall into this circle
+center point should not be duplicated
+"""
+
+import clr
+clr.AddReference("mtrand")
+
+import math
+import operator 
+import numpy as np
+from numpy import linalg as LA
+import Rhino.Geometry as RG
+
+#=========
+# Create Fixed Distance Neighbourhoods
+#=========
+
+def getNeighboursInRange(PC, R):
+    """
+    Get the neighbours in range R
+    Input:
+        PC: point cloud as list of points (Point3d)
+        R: the range/radius (float)
+    Output:
+        list: list: [reference point, pt_1, pt_2, ..., pt_k]
+    """
+    # create a list of lists of points+neihborhoods as [[point_n, neighbors],[],...]
+    FDN_list = [[] for x in xrange(len(PC))]
+    for pt in range(len(PC)):
+        p = PC[pt]
+        # first element in the sublist is the reference point in the neihborhood
+        FDN_list[pt].append(PC[pt])
+        neighbours = []
+        for p2 in range(len(PC)):
+            # calculate distance for each point in PC
+            if p.DistanceTo(PC[p2]) < R:
+                neighbours.append(PC[p2])
+        # the following elements are the neighbors
+        [FDN_list[pt].append(i) for i in neighbours]
+    return FDN_list
+
+FDN_list = getNeighboursInRange(PC,R)
+a = FDN_list
+
+#=======================
+# Estimation of normals
+#=======================
+covM = [] # the list of covariance matrices
+
+for Pgroup in range(len(FDN_list)):
+    if len(FDN_list[Pgroup]) <= 1:
+        continue
+    else:
+        point_a = np.empty([len(FDN_list[Pgroup]),3])
+        for point in range(len(FDN_list[Pgroup])):
+            point_a[point][0] = FDN_list[Pgroup][point][0] # X
+            point_a[point][1] = FDN_list[Pgroup][point][1] # Y
+            point_a[point][2] = FDN_list[Pgroup][point][2] # Z
+            m = np.cov(point_a, rowvar=0)
+            if np.isnan(np.sum(m)) or np.isinf(np.sum(m)):
+                break
+            else:
+                #each column represents a variable, while the rows contain observations
+                covM.append(m)
+
+normals = []
+for m in range(len(FDN_list)):
+   # calculate eigenvalues (e_vals) and eigenvectors (e_vecs)
+   e_vals, e_vecs = LA.eig(covM[m])
+   # get the smallest eigenvalue
+   eMin = np.argmin(e_vals)
+   # retrieve the smallest eigenvector corresponding to the smallest eigenvalue
+   normal = e_vecs[eMin]
+   # cast normal coordinates into floats
+   normal = [float(i) for i in normal]
+   # create a Vector3d from each normal
+   normals.append(RG.Vector3d(normal[0],normal[1],normal[2])) 
+   
+N = normals
+
+#=======================
+# Check if the direction of the normals are in align with the point of origin of the LIDAR laser beam
+normals_copy = normals[:]
+for i in range(len(normals_copy)):
+    v = normals_copy[i]
+    # unitize normals
+    v.Unitize()
+    # compute dot product of Z unit vector and normal
+    dp = RG.Vector3d.Multiply(Z,v) 
+    if dp < 0.0:
+        # if dot product negative, reverse vector
+        normals[i].Reverse() 
+
+#=======================
+# Estimation of surface curvature
+#=======================
+curvature = []
+for n in normals:
+    c = n[0]/(n[0]+n[1]+n[2])
+    curvature.append(c)
+    
+Cur = curvature
---
+++ kNN.py
@@ -0,0 +1,98 @@
+"""
+Reference: http://machinelearningmastery.com/tutorial-to-implement-k-nearest-neighbors-in-python-from-scratch/
+"""
+import clr
+clr.AddReference("mtrand")
+
+import math
+import operator 
+import numpy as np
+from numpy import linalg as LA
+import Rhino.Geometry as RG
+
+#=========
+# Get the k-Nearest Neighbours
+#=========
+
+def getNeighbors(PC, point, k):
+    """
+    Calculates the k-Nearest Neighbours(k) for each point(point) in a point cloud(PC)
+    Input: 
+        PC: Point Cloud as list of points (Point3d )
+        point: a single reference point (Point3d)
+        k: number of points in the neihborhood
+    Output:
+        list: [reference point, pt_1, pt_2, ..., pt_k]
+    """
+    distances = []
+    for pt in range(len(PC)):
+        dist = point.DistanceTo(PC[pt])
+        distances.append((PC[pt], dist))
+    # sort the distances in ascending order
+    distances.sort(key=operator.itemgetter(1)) 
+    neighbors = []
+    for x in range(k):
+        # retrieve the shortest distance
+        neighbors.append(distances[x][0])
+    return neighbors
+
+# create a list of lists of points+neihborhoods as [[point_n, neighbors],[],...]   
+kNN_list = [[] for x in xrange(len(PC))] 
+for point in range(len(PC)):
+    # first element in the sublist is the reference point in the neihborhood
+    kNN_list[point].append(PC[point])
+    neighbors = getNeighbors(PC, PC[point], k)
+    # the following elements are the neighbors
+    [kNN_list[point].append(i) for i in neighbors]
+    
+a = kNN_list
+
+#=======================
+# Estimation of normals
+#=======================
+covM = [] # the list of covariance matrices
+point_a = np.empty([len(kNN_list[0]),3]) # a single empty array shaped as an input for the covariance matrix
+
+for Pgroup in range(len(kNN_list)):
+    for point in range(len(kNN_list[Pgroup])):
+        point_a[point][0] = kNN_list[Pgroup][point][0] # X
+        point_a[point][1] = kNN_list[Pgroup][point][1] # Y
+        point_a[point][2] = kNN_list[Pgroup][point][2] # Z
+        m = np.cov(point_a, rowvar=0) #each column represents a variable, while the rows contain observations
+        covM.append(m)
+
+normals = []
+for m in range(len(kNN_list)):
+   # calculate eigenvalues (e_vals) and eigenvectors (e_vecs)
+   e_vals, e_vecs = LA.eig(covM[m])
+   # get the smallest eigenvalue
+   eMin = np.argmin(e_vals)
+   # retrieve the smallest eigenvector corresponding to the smallest eigenvalue
+   normal = e_vecs[eMin]
+   # cast normal coordinates into floats
+   normal = [float(i) for i in normal]
+   # create a Vector3d from each normal
+   normals.append(RG.Vector3d(normal[0],normal[1],normal[2])) 
+N = normals
+
+#=======================
+# Check if the direction of the normals are in align with the point of origin of the LIDAR laser beam
+normals_copy = normals[:]
+for i in range(len(normals_copy)):
+    v = normals_copy[i]
+    # unitize normals
+    v.Unitize()
+    # compute dot product of Z unit vector and normal
+    dp = RG.Vector3d.Multiply(Z,v) 
+    if dp < 0.0:
+        # if dot product negative, reverse vector
+        normals[i].Reverse() 
+
+#=======================
+# Estimation of surface curvature
+#=======================
+curvature = []
+for n in normals:
+    c = n[0]/(n[0]+n[1]+n[2])
+    curvature.append(c)
+Cur = curvature
